55ca60564000969256dea41a7a657718d98be165
shivam1101a <38149931+shivam1101a@users.noreply.github.com>
2019-03-23 23:34:15 +0530

    Add files via upload

---
+++ SentimentAnalysis.r
@@ -0,0 +1,36 @@
+install.packages("twitteR")
+install.packages("purrr")
+install.packages("ROAuth")
+install.packages("RCurl")
+require('ROAuth')
+require('RCurl')
+install.packages("plyr")
+install.packages("stringr")
+library(twitteR)
+library(purrr)
+library(plyr)
+library(stringr)
+
+##SENTIMENT FUNTION
+
+score.sentiment <- function(sentences, pos.words, neg.words, .progress='none')
+{
+  require(plyr)
+  require(stringr)
+  score<-laply(sentences,function(sentence, pos.words, neg.words){
+    sentence<-gsub('[[:punct:]]', "",sentence)
+    sentence<-gsub('[[:cntrl:]]', "",sentence)
+    sentence<-gsub('\\d+',"",sentence)
+    sentence<-tolower(sentence)
+    word.list<-str_split(sentence,'\\s+')
+    words<-unlist(word.list)
+    pos.matches<-match(words,pos.words)
+    neg.matches<-match(words,neg.words)
+    pos.matches<-!is.na(pos.matches)
+    neg.matches<-!is.na(neg.matches)
+    score<-sum(pos.matches)-sum(neg.matches)
+    return(score)
+  },pos.words,neg.words, .progress=.progress)
+  score.df<-data.frame(score=scores,text=sentences)
+  return(scores.df)
+}
